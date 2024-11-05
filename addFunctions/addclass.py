49cfa4286862b07acd99f989f2b33ed82dd26d0e
D1158429 <D1158429@o365.fcu.edu.tw>
2024-11-05 23:15:34 +0800

    add class

---
+++ addFunctions/addclass.py
@@ -0,0 +1,48 @@
+import sqlite3
+from datetime import time
+
+# con = sqlite3.connect("../instance/database.db")
+con = sqlite3.connect("instance/database.db")
+cur = con.cursor()
+
+classes = [
+    (
+        1,
+        "作業系統",
+        "陳烈武",
+        "資電104",
+        "13:10",
+    ),
+    (
+        2,
+        "微處理機實習",
+        "王益文",
+        "資電234",
+        "09:10",
+    ),
+    (
+        3,
+        "微處理機",
+        "王益文",
+        "資電404",
+        "14:10",
+    ),
+    (
+        4,
+        "軟體工程",
+        "許懷中",
+        "資電330",
+        "13:10",
+    ),
+    (
+        5,
+        "資料科學實務",
+        "許懷中",
+        "紀207",
+        "10:10",
+    ),
+]
+cur.executemany(
+    "INSERT INTO classes VALUES (?, ?, ?, ?, ?)", classes
+)
+con.commit()
--- website/models.py
+++ website/models.py
@@ -3,7 +3,6 @@ from . import db
 from sqlalchemy.sql import func
 from flask_sqlalchemy import SQLAlchemy
 
-
 class Student(db.Model, UserMixin):
     __tablename__ = "students"
 
@@ -22,3 +21,19 @@ class Student(db.Model, UserMixin):
 
     def __repr__(self):
         return f"id: {self.student_id}, email: {self.account}, name: {self.name}"
+
+class Classes(db.Model):
+    __tablename__ = "classes"
+
+    def __init__(self, id, name, teacher, classroom, time):
+        self.id = id
+        self.name = name
+        self.teacher = teacher
+        self.classroom = classroom
+        self.time = time
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    teacher = db.Column(db.String(50), nullable=False)
+    classroom = db.Column(db.String(50), nullable=False)
+    time = db.Column(db.String(50), nullable=False)
