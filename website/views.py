634546a315b76a8a33107d626b39f35c2997d458
weber <weber22517@gmail.com>
2024-11-13 15:07:21 +0800

    Update views.py

--- website/views.py
+++ website/views.py
@@ -99,6 +99,7 @@ def add_class():
     db.session.commit()
 
     flash(f"課程 {new_class.name} 已成功加入", "success")
+    
 
 
     return redirect(url_for('views.search', search_query=request.form.get('search_query', '')))
