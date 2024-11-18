191d86e3529fb7a5b319485950d7f6758a27b0ee
weber <weber22517@gmail.com>
2024-11-18 20:44:52 +0800

    新增課程大綱
    在查詢課程顯示結果新增查看課程大綱

--- addFunctions/addclass.py
+++ addFunctions/addclass.py
@@ -8,57 +8,62 @@ cur = con.cursor()
 courses = [
     (
         1,
-        "1234",
+        "1428",
         "作業系統",
         "陳烈武",
         "資電104",
         "四",
         "6,7",
         3,
+        "https://ilearntools.fcu.edu.tw/W320104/W320104_SyllabusFullVer.aspx?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjoicHVibGljIiwibGFuZyI6ImNodCIsImNvdXJzZUlkIjoiMTEzMUNFMDcxMzQxNjcxMDAwMSIsImNvdXJzZUtleSI6eyJ5bXNfeWVhciI6MTEzLCJ5bXNfc21lc3RlciI6MSwiY2xzX2lkIjoiQ0UwNzEzNCIsInN1Yl9pZCI6IjE2NzEwIiwic2NyX2R1cCI6IjAwMSJ9LCJzeXNUeXBlIjpudWxsLCJ1bml0IjpudWxsLCJyb2xlIjpudWxsLCJleHAiOjE3MzE5NDM3NzZ9.YS-LGZqtyuvacP837VLqzt9w_MhB7f0-Oz_khvZGqwk#!/fullVer/cht",
     ),
     (
         2,
-        "9487",
+        "1430",
         "微處理機實習",
         "王益文",
         "資電234",
         "一",
         "2,3,4",
         1,
+        "https://ilearntools.fcu.edu.tw/W320104/W320104_SyllabusFullVer.aspx?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjoicHVibGljIiwibGFuZyI6ImNodCIsImNvdXJzZUlkIjoiMTEzMUNFMDcxMzQ2MDYwMTAwMSIsImNvdXJzZUtleSI6eyJ5bXNfeWVhciI6MTEzLCJ5bXNfc21lc3RlciI6MSwiY2xzX2lkIjoiQ0UwNzEzNCIsInN1Yl9pZCI6IjYwNjAxIiwic2NyX2R1cCI6IjAwMSJ9LCJzeXNUeXBlIjpudWxsLCJ1bml0IjpudWxsLCJyb2xlIjpudWxsLCJleHAiOjE3MzE5NDM4Njl9.hikShazJUMBFO6jAz-kA32dhUI0otuntg0W2a9jOKsY#!/fullVer/cht",
     ),
     (
         3,
-        "5566",
+        "1429",
         "微處理機",
         "王益文",
         "資電404",
         "一",
         "7,8",
         3,
+        "https://ilearntools.fcu.edu.tw/W320104/W320104_SyllabusFullVer.aspx?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjoicHVibGljIiwibGFuZyI6ImNodCIsImNvdXJzZUlkIjoiMTEzMUNFMDcxMzQ2MDYwMDAwMSIsImNvdXJzZUtleSI6eyJ5bXNfeWVhciI6MTEzLCJ5bXNfc21lc3RlciI6MSwiY2xzX2lkIjoiQ0UwNzEzNCIsInN1Yl9pZCI6IjYwNjAwIiwic2NyX2R1cCI6IjAwMSJ9LCJzeXNUeXBlIjpudWxsLCJ1bml0IjpudWxsLCJyb2xlIjpudWxsLCJleHAiOjE3MzE5NDM4Njh9.0T2f5JF2m8aWUOFCGxntzr_DRTO1ApONw3ZWWdZZVDs#!/fullVer/cht",
     ),
     (
         4,
-        "4321",
+        "1432",
         "軟體工程",
         "許懷中",
         "資電330",
         "三",
         "6,7,8",
         3,
+        "https://ilearntools.fcu.edu.tw/W320104/W320104_SyllabusFullVer.aspx?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjoicHVibGljIiwibGFuZyI6ImNodCIsImNvdXJzZUlkIjoiMTEzMUNFMDcxMzk0NDIzMzAwMSIsImNvdXJzZUtleSI6eyJ5bXNfeWVhciI6MTEzLCJ5bXNfc21lc3RlciI6MSwiY2xzX2lkIjoiQ0UwNzEzOSIsInN1Yl9pZCI6IjQ0MjMzIiwic2NyX2R1cCI6IjAwMSJ9LCJzeXNUeXBlIjpudWxsLCJ1bml0IjpudWxsLCJyb2xlIjpudWxsLCJleHAiOjE3MzE5NDM5Nzl9.EVZuK0eDzbyF3J5fyOvKh7t9pUFuZwSo-YgVsA4S4Sw#!/fullVer/cht",
     ),
     (
         5,
-        "1111",
+        "1434",
         "資料科學實務",
         "許懷中",
         "紀207",
         "二",
         "3,4",
         3,
+        "https://ilearntools.fcu.edu.tw/W320104/W320104_SyllabusFullVer.aspx?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjoicHVibGljIiwibGFuZyI6ImNodCIsImNvdXJzZUlkIjoiMTEzMUNFMDcxMzk1OTQ3MjAwMSIsImNvdXJzZUtleSI6eyJ5bXNfeWVhciI6MTEzLCJ5bXNfc21lc3RlciI6MSwiY2xzX2lkIjoiQ0UwNzEzOSIsInN1Yl9pZCI6IjU5NDcyIiwic2NyX2R1cCI6IjAwMSJ9LCJzeXNUeXBlIjpudWxsLCJ1bml0IjpudWxsLCJyb2xlIjpudWxsLCJleHAiOjE3MzE5NDQwMjN9.lHL7P8f7k1m1Yoaafn9X9F_2y7dJDGjNVEPi88A24NA#!/fullVer/cht",
     ),
 ]
 cur.executemany(
-    "INSERT INTO courses VALUES (?, ?, ?, ?, ?, ?, ?, ?)", courses
+    "INSERT INTO courses VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", courses
 )
 
 # enrollments = [
